4f7c07c38fde6a58100e58f10caab52e10828a1f
ewertonsvb1 <83789031+ewertonsvb1@users.noreply.github.com>
2023-09-20 11:12:20 -0300

    Commit  Aulas 6,7 e exercicios

---
+++ aula06A.py
@@ -0,0 +1,7 @@
+n1 = int(input('primeiro número:'))
+n2 = int(input('segundo número:'))
+
+r = n1 + n2
+print(r)
+
+print('tipo:', type(r))
---
+++ aula07.py
@@ -0,0 +1,58 @@
+"""
+#5**2 == 25
+5//2 == 2
+5%2 == 1
+
+
+# Ordem de resolução
+
+1 ()
+2 **
+3 * / // %
+4 + - """
+
+"""
+n1 = int(input('primeiro numero'))
+n2 = int(input('segundo numero'))
+
+s = n1 + n2
+su = n1 - n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+
+print('Soma = {} ,  Subtração = {} , Multiplicação = {} , Divisão = {:.3f} , Divisão Inteiro = {}, Expo = {}' .format(s, su, m, d, di, e)) """
+
+
+#Exercicio 1
+
+"""
+n1 = int(input('Escolha um número:'))
+print('Antecessor: {} , Sucessor: {}'.format(n1 - 1,(n1 + 1))) """
+
+#Exercicio 2
+
+"""
+n1 = int(input('Escolha um número:'))
+print('Dobro: {} , Triplo: {}, Raiz Quadrada: {}'.format(n1 * 2,(n1 * 3),(n1 * n1))) """
+
+#Exercicio 3
+
+""" 
+n1 = int(input('primeiro número:'))
+n2 = int(input('segundo número:'))
+n3 = int(input('segundo número:'))
+n4 = int(input('segundo número:'))
+#media = (n1 + n2 + n3 + n4)/4
+
+print('Média {}' .format((n1 + n2 + n3 + n4)/4))"""
+
+#Exercicio 4
+
+"""
+n1 = float(input("informe a medida em metros: "))
+print('CM: {:.0f}, MM: {:.0f}'.format((n1*100),(n1*1000))) """
+
+
+
