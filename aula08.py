6911e7e753bfc789cd435c252348eed3d2213eee
ewertonsvb1 <83789031+ewertonsvb1@users.noreply.github.com>
2023-09-21 13:32:32 -0300

    Utilizando módulos

---
+++ aula08.py
@@ -0,0 +1,66 @@
+"""
+1 - import bebida suco, refri, café   ( importo a biblioteca toda )
+
+2 - from bebida import refri ( importo só que preciso para usar )
+
+
+
+biblioteca math
+
+ import math
+
+1 - ceil  - arredonda para cima
+2 - floor - arredonda para baixo
+3 - trunc - eliminia da vírgula para frente
+4 - pow - potência **
+5 - sqrt - raiz
+6 - factorial
+
+from math import sqrt  ( só importa a dá raiz )
+"""
+
+
+
+# import math
+# n1 = int(input("digite um numero"))
+# raiz = math.sqrt(n1)
+# print ('Raiz de {} é = {}'.format(n1, math.ceil(raiz)))
+
+
+# from math import sqrt , floor
+# n1 = int(input("digite um numero"))
+# raiz = sqrt(n1)
+# print ('raiz de  {} é = {:.0f}' .format(n1, floor(raiz)))
+
+
+# exemplo de import
+# import random
+# nome ='João','Maria', 'José', 'Carlos'
+# n1 = random.choice(nome)
+# print('Ganhador: {}'.format(n1))
+
+# import random
+#
+# respostas = []
+#
+# for _ in range(4):
+#     resposta = input('O costa é o que? ')
+#     respostas.append(resposta)
+#
+# resposta_escolhida = random.choice(respostas)
+# print('Costa  {}'.format(resposta_escolhida))
+
+
+
+#exercicio 16
+
+# from math import trunc
+# n1 = float(input("Digite um numero"))
+# print('A parte inteira de {} é : {}' .format(n1,trunc(n1)))
+
+#OU
+
+# n1 = float(input("Digite um numero"))
+# print('A parte inteira de {} é : {:.0f}' .format(n1,int(n1)))
+
+
